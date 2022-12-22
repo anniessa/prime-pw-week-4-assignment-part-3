7662e464847d6729d491841a41f014e915b8ace3
Anniessa Antar <anniessa.antar@gmail.com>
2022-12-22 11:11:23 -0600

    trying to put isFull function in the addItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,15 +1,21 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-let basket = ['apples', 'oranges', 'bananas', 'grapes', 'strawberries'];
+let basket = ['blackberries', 'oranges', 'bananas', 'grapes'];
 
+const maxItems = 5;
+
+function isFull() {
+    return basket.length >= maxItems;
+}
 function addItem(item) {
-basket.push(item);
-return true;
+return basket.push(item) ? isFull() : false
 }
 console.log(`Basket is ${basket}`);
 console.log('Adding apples (expect true)', addItem('apples'));
 console.log(`Basket is now ${basket}`);
+console.log('There is no more room in your basket (expect fasle)', isFull('potatoes'));
+
 
 function listItems(){
     for (const item of basket){
