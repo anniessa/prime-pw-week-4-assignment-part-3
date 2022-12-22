b0cead13b0bdbe4320b9d7e6f64683c78a0f7da0
Anniessa Antar <anniessa.antar@gmail.com>
2022-12-22 10:41:19 -0600

    created a variable basket that is an array, created a function called addItem which adds an item to the array basket, created a function calle dlistItem whcih is a for-of loop that logs each item in the basket array. created a function called empty that reset the basket array.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,33 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = ['apples', 'oranges', 'bananas', 'grapes', 'strawberries'];
+
+function addItem(item) {
+basket.push(item);
+return true;
+}
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+function listItems(){
+    for (const item of basket){
+        console.log(item);
+    }
+}
+listItems();
+
+function empty () {
+basket = [];
+}
+empty();
+console.log(basket);
+
+const maxItems = 5;
+
+function isFull() {
+    return basket.length >= maxItems;
+}
+console.log(isFull());
+
